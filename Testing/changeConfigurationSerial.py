36986749317295401b18014149d1131e5a724c49
Jaimi5 <janms2009@gmail.com>
2023-06-19 20:12:10 +0200

    Added possible graph adjacency for testing

--- Testing/changeConfigurationSerial.py
+++ Testing/changeConfigurationSerial.py
@@ -85,6 +85,117 @@ class ChangeConfigurationSerial:
             with open(srcFile, "w") as file:
                 file.write(srcData)
 
+    def changeAdjacencyGraph(self):
+        # Read the file
+        with open(self.fileName, "r") as file:
+            data = file.read()
+
+        # Parse the file
+        json_data = json.loads(data)
+
+        for environment in self.environments:
+            # Find the LoRaMesher src file given the environment.
+            srcFile = os.path.join(
+                os.path.dirname(__file__),
+                "..",
+                ".pio",
+                "libdeps",
+                environment,
+                "LoRaMesher",
+                "src",
+                "BuildOptions.h",
+            )
+
+            with open(srcFile, "r") as file:
+                srcData = file.read()
+
+            # Define that we are in a test environment
+            # Find the line where the LoRaMesher is defined and change it
+            for line in srcData.splitlines():
+                if line.find("#define TESTING") != -1:
+                    srcData = srcData.replace(line, "#define TESTING")
+                    break
+
+            # Save the file
+            with open(srcFile, "w") as file:
+                file.write(srcData)
+
+        for environment in self.environments:
+            # Find the LoRaMesher src file given the environment.
+            srcFile = os.path.join(
+                os.path.dirname(__file__),
+                "..",
+                ".pio",
+                "libdeps",
+                environment,
+                "LoRaMesher",
+                "src",
+                "LoraMesher.cpp",
+            )
+
+            with open(srcFile, "r") as file:
+                srcData = file.readlines()
+
+            # Find the function where the LoRaMesher adjacency graph is defined and change it. It is called canReceivePacket
+            # Clear the function contents
+            start_line = -1
+            end_line = -1
+            for i, line in enumerate(srcData):
+                if line.find("bool LoraMesher::canReceivePacket(") != -1:
+                    start_line = i
+                if start_line != -1 and line.strip() == "#endif":
+                    end_line = i - 1
+                    break
+
+            if start_line >= 0 and end_line >= 0:
+                # Clear function contents
+                del srcData[start_line + 1 : end_line]
+
+                adjacencyGraphInCpp = ""
+
+                if json_data["LoRaMesherAdjacencyGraph"] == []:
+                    adjacencyGraphInCpp = "\treturn true;"
+
+                else:
+                    adjacencyGraphInCpp = (
+                        "\tuint16_t localAddress = getLocalAddress();\n"
+                    )
+
+                    # Iterate through the adjacency graph and add it to the cpp file
+                    for i in range(0, len(json_data["LoRaMesherAdjacencyGraph"])):
+                        adjacencyGraphInCpp += (
+                            "\tif (localAddress == "
+                            + str(json_data["LoRaMesherAdjacencyGraph"][i]["id"])
+                            + ") {\n"
+                        )
+                        for neighbor in json_data["LoRaMesherAdjacencyGraph"][i][
+                            "neighbors"
+                        ]:
+                            adjacencyGraphInCpp += (
+                                "\t\tif (source == " + str(neighbor["to"]) + ") {\n"
+                            )
+                            adjacencyGraphInCpp += (
+                                "\t\t\treturn "
+                                + str(neighbor["distance"] == "1").lower()
+                                + ";\n"
+                            )
+                            adjacencyGraphInCpp += "\t\t}\n"
+
+                        adjacencyGraphInCpp += "\t\treturn false;\n"
+                        adjacencyGraphInCpp += "\t}\n"
+
+                    adjacencyGraphInCpp += "\treturn false;\n"
+
+                # Add new contents
+                srcData.insert(
+                    start_line + 1,
+                    adjacencyGraphInCpp,
+                )
+
+                # Write new content to the file
+                with open(srcFile, "w") as file:
+                    file.writelines(srcData)
+
     def startSim(self):
         os.system("python simulation.py")
 
@@ -93,10 +204,11 @@ if __name__ == "__main__":
     configFile = "simulationTest"
     os.makedirs("simulationTest", exist_ok=True)
 
-    simConfig = simConfiguration.SimConfiguration(configFile)
+    simConfig = simConfiguration.SimConfiguration(configFile, "simulationTest")
 
     changeConfiguration = ChangeConfigurationSerial(
         simConfig.getFileName(), ["ttgo-t-beam", "ttgo-lora32-v1"]
     )
-    changeConfiguration.changeSimulatorApp()
-    changeConfiguration.changeLoRaMesher()
+    changeConfiguration.changeAdjacencyGraph()
+    # changeConfiguration.changeSimulatorApp()
+    # changeConfiguration.changeLoRaMesher()
--- Testing/simConfiguration.py
+++ Testing/simConfiguration.py
@@ -12,7 +12,7 @@ class SimConfiguration:
 
     def getName(self):
         return self.Name
-    
+
     def copyConfiguration(self, directory):
         # Copy the file "simConfiguration.json" to the new directory
         with open(self.fileName, "r") as file:
@@ -50,10 +50,68 @@ class SimConfiguration:
                 if value != "":
                     json_data[key] = value
 
+        # Add the adjacency graph
+        json_data["LoRaMesherAdjacencyGraph"] = self.getAdjacencyGraph()
+
         # Save the file
         with open(self.fileName, "w") as file:
             file.write(json.dumps(json_data, indent=4))
 
+    def getAdjacencyGraph(self):
+        # Ask the user if he wants to add a an adjacency graph
+        addAdjacencyGraph = input("Do you want to add an adjacency graph? (y/n): ")
+        while addAdjacencyGraph != "y" and addAdjacencyGraph != "n":
+            addAdjacencyGraph = input("Do you want to add an adjacency graph? (y/n): ")
+
+        if addAdjacencyGraph == "n":
+            return []
+
+        # Ask the user how many nodes he wants to add to the adjacency graph
+        numberOfNodes = int(
+            input("How many nodes do you want to add to the adjacency graph?: ")
+        )
+
+        # Ask the user the id of the nodes
+        nodes = []
+        for i in range(numberOfNodes):
+            nodes.append(input("Node " + str(i) + " id: "))
+
+        # Ask the user the adjacency matrix
+        adjacencyMatrix = []
+
+        for i in range(numberOfNodes):
+            adjacencyMatrix.append([])
+            for j in range(numberOfNodes):
+                adjacencyMatrix[i].append(
+                    input("Node " + str(i) + " to node " + str(j) + " distance: ")
+                )
+
+        # Create the adjacency graph
+        adjacencyGraph = []
+
+        for i in range(numberOfNodes):
+            neighbors = []
+            for j in range(numberOfNodes):
+                if adjacencyMatrix[i][j] != "":
+                    neighbors.append(
+                        {"to": nodes[j], "distance": adjacencyMatrix[i][j]}
+                    )
+            adjacencyGraph.append({"id": nodes[i], "neighbors": neighbors})
+
+        return adjacencyGraph
+
+    def printAdjacencyGraph(self):
+        # Read the file
+        with open(self.fileName, "r") as file:
+            data = file.read()
+
+        # Parse the file
+        json_data = json.loads(data)
+
+        # Print the adjacency graph
+        print(json.dumps(json_data["LoRaMesherAdjacencyGraph"], indent=4))
+
 
 if __name__ == "__main__":
-    simConfiguration = SimConfiguration(os.path.dirname(__file__))
+    simConfiguration = SimConfiguration(os.path.dirname(__file__), "simConfiguration")
+    simConfiguration.createConfiguration()
