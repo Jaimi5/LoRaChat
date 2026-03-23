35d19afdd83923454ac91fdb54cf0274f8782cd6
Jaimi5 <janms2009@gmail.com>
2026-03-23 17:26:34 +0100

    testing new LM configuration

--- src/config.h
+++ src/config.h
@@ -303,18 +303,18 @@
 #endif
 #endif
 
-#define LORA_MANAGER_ID 0x3ADF
+#define LORA_MANAGER_ID 0x006C
 
 // LoRa RF Parameters
 #define LORA_FREQUENCY 869.900F
-#define LORA_SPREADING_FACTOR 7U
+#define LORA_SPREADING_FACTOR 9U
 #define LORA_BANDWIDTH 125.0
 #define LORA_CODING_RATE 7U
-#define LORA_POWER 6
+#define LORA_POWER 10
 #define LORA_SYNC_WORD 20U      // Network identifier (0-255)
 #define LORA_CRC true           // Enable CRC checking
 #define LORA_PREAMBLE_LENGTH 8U
-#define LORA_DUTY_CYCLE 0.01f
+#define LORA_DUTY_CYCLE 1.0f
 #define LORA_MAX_PACKET_SIZE 255
 #define LORA_MIN_SLEEP_FRACTION 0
 #ifdef USE_LORAMESHER_V2
